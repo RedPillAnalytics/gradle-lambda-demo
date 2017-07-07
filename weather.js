67736ce26ea74e8a536dab2090d4c5513f88dcd7
Emily Carlsen <emilytcyt@gmail.com>
2017-07-07 13:30:06 -0400

    Separate zip param and change to hourly data

--- weather.js
+++ weather.js
@@ -1,8 +1,11 @@
 const fetch = require('node-fetch')
 const keys = require('./keys.js')
+const zip = 30144
 
-fetch('http://api.wunderground.com/api/' + keys.wunderground + '/conditions/q/30144.json').then((res) => {
-  return res.text()
-}).then((body) => {
-  console.log(body)
+fetch('http://api.wunderground.com/api/' + keys.wunderground + '/hourly/q/' + zip + '.json').then((res) => {
+  return res.json()
+}).then((json) => {
+  for(let i = 0; i < json.hourly_forecast.length; i++) {
+    console.log(json.hourly_forecast[i].temp.english)
+  }
 })
